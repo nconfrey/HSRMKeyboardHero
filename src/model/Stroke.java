94333cce1d5d56d6aeed2a4c69844925a105f6e3
Martin Juhasz <info@martinjuhasz.de>
2013-12-12 13:12:59 +0100

    changed model

--- src/model/Stroke.java
+++ src/model/Stroke.java
@@ -1,19 +1,13 @@
 package model;
 
-public enum Stroke {
-	F1((byte)(1<<0)),
-	F2((byte)(1<<1)),
-	F3((byte)(1<<2)),
-	F4((byte)(1<<3)),
-	F5((byte)(1<<4));
+public class Stroke {
 	
-	private final byte value;
+	private StrokeKey key;
+	private int length;
 	
-	private Stroke(byte value) {
-		this.value = value;
+	public Stroke(StrokeKey strokeKey, int length) {
+		this.key = strokeKey;
+		this.length = length;
 	}
-	
-	public byte getValue() {
-		return value;
-	}
+
 }
--- src/model/StrokeSet.java
+++ src/model/StrokeSet.java
@@ -2,31 +2,24 @@ package model;
 
 import java.util.HashMap;
 import java.util.Map;
-import model.Stroke;
+import model.StrokeKey;
 
 public class StrokeSet {
 	
-	private Map<Integer, Byte> strokes;
+	private Map<Integer, Stroke> strokes;
 	
 	public StrokeSet() {
 		this.strokes = new HashMap<>();
 	}
 	
-	public boolean isSet(int frame, Stroke stroke) {
-		if (!strokes.containsKey(frame)){
-			return false;
-		}
-		
-		return (strokes.get(frame) & stroke.getValue()) != 0;
-	}
-	
-	public void set(int frame, Stroke stroke) {
-		byte strokeSet = 0;
+	public Stroke set(int frame, StrokeKey strokeKey) {
 		if(strokes.containsKey(frame)) {
-			strokeSet = strokes.get(frame);
+			return strokes.get(frame);
 		}
 		
-		strokes.put(frame, (byte)(strokeSet | stroke.getValue()));
+		Stroke newStroke = new Stroke(strokeKey, 0);
+		strokes.put(frame, newStroke);
+		return newStroke;
 	}
 	
 }
