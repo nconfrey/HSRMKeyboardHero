09eb6872093deb9d32aefa0ba0835d79421cef9c
Moritz Moeller <mobbitz@gmail.com>
2014-01-16 14:39:09 +0100

    added songviewer class

--- src/model/PersistenceHandler.java
+++ src/model/PersistenceHandler.java
@@ -1,11 +1,14 @@
 package model;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.net.URISyntaxException;
 
+import controller.KeyboardHero;
 import controller.player.Playlist;
 
 /**
@@ -15,18 +18,18 @@ import controller.player.Playlist;
 
 public class PersistenceHandler {
 
-    private static final String FILE_PATH = "/tmp/playlist.mpl";
-
+    //private static final String FILE_PATH = KeyboardHero.class.getResource("/"+mp3Name).toURI();
+	private static final File playlistFile = new File(System.getProperty("user.home") + File.separator + "playlist.mpl");
     /**
      * Save a playlist to disk.
      *
      * @param playlist the playlist to save
      */
     public static void savePlaylist(Playlist playlist) {
-        try (FileOutputStream fileOut = new FileOutputStream(FILE_PATH);
+        try (FileOutputStream fileOut = new FileOutputStream(playlistFile);
              ObjectOutputStream out = new ObjectOutputStream(fileOut)) {
             out.writeObject(playlist);
-            System.out.printf("Serialized data is saved in " + FILE_PATH);
+            System.out.printf("Serialized data is saved");
         } catch (IOException i) {
             i.printStackTrace();
         }
@@ -40,10 +43,10 @@ public class PersistenceHandler {
     public static Playlist loadPlaylist() {
         Playlist playlist = null;
 
-        try (FileInputStream fileIn = new FileInputStream(FILE_PATH);
+        try (FileInputStream fileIn = new FileInputStream(playlistFile);
              ObjectInputStream in = new ObjectInputStream(fileIn)) {
             playlist = (Playlist) in.readObject();
-            System.out.println("Playlist: " + FILE_PATH + " loaded");
+            System.out.println("Playlist: loaded");
 
             // TODO: implemnt
             // playlist.checkConsistency();
