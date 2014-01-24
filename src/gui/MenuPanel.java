582e390bd6662b64e8c3d4dfce5ff34b63ae6797
Martin Juhasz <info@martinjuhasz.de>
2014-01-24 20:04:40 +0100

    main menu design

--- src/gui/MenuPanel.java
+++ src/gui/MenuPanel.java
@@ -1,10 +1,16 @@
 package gui;
 
+import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JPanel;
+
+import view.ImagePanel;
+import view.MenuButton;
+import net.miginfocom.swing.MigLayout;
 
 public class MenuPanel extends GHPanel implements ActionListener{
 	
@@ -15,22 +21,29 @@ public class MenuPanel extends GHPanel implements ActionListener{
 	
 	public MenuPanel(){
 		
-		this.setLayout(new GridLayout(4,1));
+		this.setBackground(Color.WHITE);
+		this.setLayout(new MigLayout("insets 60 0 0 0, fillx"));
 		
+		this.add(new ImagePanel("logo.png", ImagePanel.SIZE_FIXED), "wrap, grow, height 157!");
 		
-		playButton = new JButton("Play");
+		playButton = new MenuButton("Play");
 		playButton.addActionListener(this);
-		recordButton = new JButton("Record");
+		recordButton = new MenuButton("Record");
 		recordButton.addActionListener(this);
-		highscoreButton = new JButton("Highscores");
+		highscoreButton = new MenuButton("Highscores");
 		highscoreButton.addActionListener(this);
-		creditsButton = new JButton("Credits");
+		creditsButton = new MenuButton("Credits");
 		creditsButton.addActionListener(this);
 		
-		this.add(playButton);
-		this.add(recordButton);
-		this.add(highscoreButton);
-		this.add(creditsButton);
+		
+		JPanel buttonPanel = new JPanel(new MigLayout("insets 0 200 20 200, fillx", "", "[]15[]"));
+		buttonPanel.setBackground(Color.WHITE);
+		buttonPanel.add(playButton, "wrap, grow, height 60!");
+		buttonPanel.add(recordButton, "wrap, grow, height 60");
+		buttonPanel.add(highscoreButton, "wrap, grow, height 60");
+		buttonPanel.add(creditsButton, "wrap, grow, height 60");
+		
+		this.add(buttonPanel, "wrap, grow");
 		
 	}
 
