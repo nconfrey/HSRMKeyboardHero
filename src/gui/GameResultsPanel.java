5c0c1bc247d81d5e54519cbe570359c47058bd6e
Martin Juhasz <info@martinjuhasz.de>
2014-01-29 13:31:45 +0100

    styled results panel

--- src/gui/GameResultsPanel.java
+++ src/gui/GameResultsPanel.java
@@ -1,6 +1,7 @@
 package gui;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -9,10 +10,13 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 import model.Score;
 import model.StrokeSet;
 import net.miginfocom.swing.MigLayout;
+import view.KeyboardHeroConstants;
+import view.MenuButton;
 
 public class GameResultsPanel extends JPanel {
 	
@@ -34,22 +38,41 @@ public class GameResultsPanel extends JPanel {
 		
 		JPanel infoPanel = new JPanel();
 		infoPanel.setLayout(new MigLayout("fillx"));
+		infoPanel.setBackground(Color.WHITE);
 		this.add(infoPanel, "grow");
 		
-		JLabel scoreLabel = new JLabel("Your Score: 12378");
-		infoPanel.add(scoreLabel, "wrap");
+		JLabel scoreTitleLabel = new JLabel("Your Score");
+		scoreTitleLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		infoPanel.add(scoreTitleLabel, "w 45%");
 		
-		JLabel bestScoreLabel = new JLabel("Best Score: 12378");
-		infoPanel.add(bestScoreLabel, "wrap");
+		JLabel bestScoreTitleLabel = new JLabel("Best Score");
+		bestScoreTitleLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		infoPanel.add(bestScoreTitleLabel, "w 45%, wrap");
 		
-		JLabel highscoreTitleLabel = new JLabel("Submit your Score:");
-		infoPanel.add(highscoreTitleLabel, "wrap");
+		JLabel scoreLabel = new JLabel("1280");
+		scoreLabel.setOpaque(true);
+		scoreLabel.setBackground(new Color(KeyboardHeroConstants.FONT_COLOR_PRIMARY));
+		scoreLabel.setForeground(Color.WHITE);
+		scoreLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		scoreLabel.setFont(new Font("sanserif", Font.BOLD, 27));
+		infoPanel.add(scoreLabel, "w 45%, h 100!");
+		
+		JLabel bestScoreLabel = new JLabel("3500");
+		bestScoreLabel.setOpaque(true);
+		bestScoreLabel.setBackground(new Color(KeyboardHeroConstants.FONT_COLOR_PRIMARY));
+		bestScoreLabel.setForeground(Color.WHITE);
+		bestScoreLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		bestScoreLabel.setFont(new Font("sanserif", Font.BOLD, 27));
+		infoPanel.add(bestScoreLabel, "w 45%, h 100!, wrap");
+		
+		JLabel highscoreTitleLabel = new JLabel("Enter your Name and submit your highscore:");
+		infoPanel.add(highscoreTitleLabel, "gapy 30,span, wrap");
 		
 		highscoreNameField = new JTextField();
-		infoPanel.add(highscoreNameField, "growx, pushx");
+		infoPanel.add(highscoreNameField, "w 45%");
 		
-		highscoreSubmitButton = new JButton("Submit");
-		infoPanel.add(highscoreSubmitButton, "wrap");
+		highscoreSubmitButton = new MenuButton("Submit");
+		infoPanel.add(highscoreSubmitButton, "w 45%, wrap");
 		highscoreSubmitButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -67,8 +90,12 @@ public class GameResultsPanel extends JPanel {
 			}
 		});
 		
+		JPanel pushPanel = new JPanel();
+		pushPanel.setBackground(Color.WHITE);
+		infoPanel.add(pushPanel, "grow, pushy, span");
 		
-		JButton playButton = new JButton("play again");
+		
+		JButton playButton = new MenuButton("play again", new Color(KeyboardHeroConstants.FONT_COLOR_SECONDARY));
 		playButton.addActionListener(new ActionListener() {
 			
 			@Override
@@ -76,9 +103,9 @@ public class GameResultsPanel extends JPanel {
 				listener.resultPanelDidSelectReplay();
 			}
 		});
-		infoPanel.add(playButton);
+		infoPanel.add(playButton, "w 45%");
 		
-		JButton closeButton = new JButton("close");
+		JButton closeButton = new MenuButton("close", new Color(KeyboardHeroConstants.FONT_COLOR_SECONDARY));
 		closeButton.addActionListener(new ActionListener() {
 			
 			@Override
@@ -86,7 +113,7 @@ public class GameResultsPanel extends JPanel {
 				listener.resultPanelShouldClose();
 			}
 		});
-		infoPanel.add(closeButton);
+		infoPanel.add(closeButton, "w 45%");
 		
 	}
 	
