57552e09558701817b13d0ccb461b1cfc13c3c81
Martin Juhasz <info@martinjuhasz.de>
2014-01-30 14:18:24 +0100

    fixed boxblur

--- src/view/BoxBlurFilter.java
+++ src/view/BoxBlurFilter.java
@@ -21,7 +21,8 @@ public class BoxBlurFilter {
 	private int hRadius = 60;
 	private int vRadius = 60;
 	private int iterations = 2;
-	
+
+
     /**
      * Filter.
      *
@@ -33,30 +34,21 @@ public class BoxBlurFilter {
         int width = src.getWidth();
         int height = src.getHeight();
 
-	/**
-	 * Filter.
-	 * 
-	 * @param src the src
-	 * @param dst the dst
-	 * @return the buffered image
-	 */
-	public BufferedImage filter(BufferedImage src, BufferedImage dst) {
-		int width = src.getWidth();
-		int height = src.getHeight();
+        if ( dst == null )
+            dst = createCompatibleDestImage( src, null );
 
-		if (dst == null)
+        int[] inPixels = new int[width*height];
         int[] outPixels = new int[width*height];
-			dst = createCompatibleDestImage(src, null);
+        getRGB( src, 0, 0, width, height, inPixels );
 
-		int[] inPixels = new int[width * height];
-		int[] outPixels = new int[width * height];
-		getRGB(src, 0, 0, width, height, inPixels);
+        for (int i = 0; i < iterations; i++ ) {
+            blur( inPixels, outPixels, width, height, hRadius );
+            blur( outPixels, inPixels, height, width, vRadius );
         }
 
-		for (int i = 0; i < iterations; i++) {
-			blur(inPixels, outPixels, width, height, hRadius);
-			blur(outPixels, inPixels, height, width, vRadius);
-		}
+        setRGB( dst, 0, 0, width, height, inPixels );
+        return dst;
+    }
 
     /**
      * Blur.
@@ -69,69 +61,48 @@ public class BoxBlurFilter {
      */
     public void blur( int[] in, int[] out, int width, int height, int radius ) {
         int widthMinus1 = width-1;
-		return dst;
-	}
+        int tableSize = 2*radius+1;
+        int divide[] = new int[256*tableSize];
 
-	/**
-	 * Blur.
-	 * 
-	 * @param in the in
-	 * @param out the out
-	 * @param width the width
-	 * @param height the height
-	 * @param radius the radius
-	 */
-	public static void blur(int[] in, int[] out, int width, int height,
-			int radius) {
-		int widthMinus1 = width - 1;
-		int tableSize = 2 * radius + 1;
-		int divide[] = new int[256 * tableSize];
+        for ( int i = 0; i < 256*tableSize; i++ )
+            divide[i] = i/tableSize;
 
         int inIndex = 0;
         
-		for (int i = 0; i < 256 * tableSize; i++)
+        for ( int y = 0; y < height; y++ ) {
             int outIndex = y;
-			divide[i] = i / tableSize;
+            int ta = 0, tr = 0, tg = 0, tb = 0;
 
             for ( int i = -radius; i <= radius; i++ ) {
-		int inIndex = 0;
+                int rgb = in[inIndex + clamp(i, 0, width-1)];
+                ta += (rgb >> 24) & 0xff;
+                tr += (rgb >> 16) & 0xff;
+                tg += (rgb >> 8) & 0xff;
+                tb += rgb & 0xff;
+            }
 
-		for (int y = 0; y < height; y++) {
-			int outIndex = y;
-			int ta = 0, tr = 0, tg = 0, tb = 0;
+            for ( int x = 0; x < width; x++ ) {
+                out[ outIndex ] = (divide[ta] << 24) | (divide[tr] << 16) | (divide[tg] << 8) | divide[tb];
 
-			for (int i = -radius; i <= radius; i++) {
-				int rgb = in[inIndex + clamp(i, 0, width - 1)];
-				ta += (rgb >> 24) & 0xff;
-				tr += (rgb >> 16) & 0xff;
-				tg += (rgb >> 8) & 0xff;
-				tb += rgb & 0xff;
-			}
-
-			for (int x = 0; x < width; x++) {
-				out[outIndex] = (divide[ta] << 24) | (divide[tr] << 16)
-						| (divide[tg] << 8) | divide[tb];
-
-				int i1 = x + radius + 1;
-				if (i1 > widthMinus1)
-					i1 = widthMinus1;
+                int i1 = x+radius+1;
+                if ( i1 > widthMinus1 )
                     i1 = widthMinus1;
-				int i2 = x - radius;
-				if (i2 < 0)
-					i2 = 0;
-				int rgb1 = in[inIndex + i1];
-				int rgb2 = in[inIndex + i2];
-
-				ta += ((rgb1 >> 24) & 0xff) - ((rgb2 >> 24) & 0xff);
-				tr += ((rgb1 & 0xff0000) - (rgb2 & 0xff0000)) >> 16;
-				tg += ((rgb1 & 0xff00) - (rgb2 & 0xff00)) >> 8;
-				tb += (rgb1 & 0xff) - (rgb2 & 0xff);
-				outIndex += height;
-			}
-			inIndex += width;
-		}
-	}
-
+                int i2 = x-radius;
+                if ( i2 < 0 )
+                    i2 = 0;
+                int rgb1 = in[inIndex+i1];
+                int rgb2 = in[inIndex+i2];
+                
+                ta += ((rgb1 >> 24) & 0xff)-((rgb2 >> 24) & 0xff);
+                tr += ((rgb1 & 0xff0000)-(rgb2 & 0xff0000)) >> 16;
+                tg += ((rgb1 & 0xff00)-(rgb2 & 0xff00)) >> 8;
+                tb += (rgb1 & 0xff)-(rgb2 & 0xff);
+                outIndex += height;
+            }
+            inIndex += width;
+        }
+    }
+        
 	/**
 	 * Sets the h radius.
 	 * 
@@ -226,30 +197,7 @@ public class BoxBlurFilter {
 			dstCM = src.getColorModel();
 		return new BufferedImage(dstCM, dstCM.createCompatibleWritableRaster(
 				src.getWidth(), src.getHeight()), dstCM.isAlphaPremultiplied(),
-    }
-    
-    /**
-     * Gets the bounds2 d.
-     *
-     * @param src the src
-     * @return the bounds2 d
-     */
-    public Rectangle2D getBounds2D( BufferedImage src ) {
-        return new Rectangle(0, 0, src.getWidth(), src.getHeight());
-    }
-    
-    /**
-     * Gets the point2 d.
-     *
-     * @param srcPt the src pt
-     * @param dstPt the dst pt
-     * @return the point2 d
-     */
-    public Point2D getPoint2D( Point2D srcPt, Point2D dstPt ) {
 				null);
-            dstPt = new Point2D.Double();
-        dstPt.setLocation( srcPt.getX(), srcPt.getY() );
-        return dstPt;
 	}
 
 	/**
