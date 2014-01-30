e4101d68a445a2eb56dc4b771b246a96fffe30f4
Moritz Moeller <mobbitz@gmail.com>
2014-01-30 19:15:19 +0100

    track from soundcloud are now marked with the icon in playmode

--- src/view/PlayListCellRenderer.java
+++ src/view/PlayListCellRenderer.java
@@ -14,22 +14,47 @@ import helper.KeyboardHeroConstants;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Font;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
 import javax.swing.DefaultListCellRenderer;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JList;
+
+import model.MP3PlayerRemoteTrack;
+import model.Track;
 
 public class PlayListCellRenderer extends DefaultListCellRenderer {
 
 	/* (non-Javadoc)
 	 * @see javax.swing.ListCellRenderer#getListCellRendererComponent(javax.swing.JList, java.lang.Object, int, boolean, boolean)
 	 */
+	
+	private ImageIcon soundCloudIcon;
+	
 	@Override
 	public Component getListCellRendererComponent(JList<? extends Object> list,
 			Object value, int index, boolean isSelected, boolean cellHasFocus) {
 		
 		Component c = super.getListCellRendererComponent(list, value,
 				index, isSelected, cellHasFocus);
+		
+		if (value instanceof Track && c instanceof JLabel) {
+			Track track = (Track) value;
+			JLabel label = (JLabel) c;
+			label.setHorizontalTextPosition(JLabel.LEFT);
+			label.setIconTextGap(15);
+			try {
+				soundCloudIcon = new ImageIcon(ImageIO.read(getClass()
+						.getResourceAsStream("/soundcloud.png")));
+			} catch (IOException e) {
+			}
+			if (track.getMp3() instanceof MP3PlayerRemoteTrack) {
+				label.setIcon(soundCloudIcon);
+			}
+		}
 		
 		c.setBackground(new Color(
 				KeyboardHeroConstants.COLOR_PRIMARY));
